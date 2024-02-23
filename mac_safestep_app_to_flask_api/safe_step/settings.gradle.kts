12392bdad69fab8f4cd8b2f2f8bd42e76f42c30c
Jae <kim_jae1@students.columbusstate.edu>
2024-02-23 12:09:53 -0500

    Everything works app connect to flask, can send get and post requests to flask

---
+++ mac_safestep_app_to_flask_api/safe_step/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "safe_step_app"
+include(":app")
